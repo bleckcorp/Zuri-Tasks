87f44f7bdb61cac35cb0850d67a9a4a9acbe0bbb
bleckcorp <bleckcorp@gmail.com>
2022-06-20 15:42:55 +0100

    commit

--- djangomodels/I4G019873IQ0/blog/models.py
+++ djangomodels/I4G019873IQ0/blog/models.py
@@ -1,3 +1,16 @@
+from turtle import title
+from unicodedata import name
 from django.db import models
+from django.contrib.auth import get_user_model
+User=get_user_model
 
 # Create your models here.
+
+class Post(models.Model):
+    title= models.CharField (max_length=200)
+    text=models.TextField
+    author= get_user_model
+    Created_date=models.DateTimeField
+    Published_date = models.DateTimeField
+
+
